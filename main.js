72f534b09a9eff64ef3c4c281c517e2de8eeb29f
Austin Casey <thepoast@gmail.com>
2019-03-02 11:36:01 -0600

    added nuggetizer

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-const firstName = 'Austin';
-const lastName = 'Casey';
-console.log(firstName + ' ' + lastName);
-console.log(`${firstName} ${lastName}`);
-console.log(firstName, lastName);
+// const firstName = 'Austin';
+// const lastName = 'Casey';
+// console.log(firstName + ' ' + lastName);
+// console.log(`${firstName} ${lastName}`);
+// console.log(firstName, lastName);
 
-const firstName0 = 'Britney';
-const lastName0 = 'Gears';
-console.log(`${firstName0} ${lastName0}`);
+// const firstName0 = 'Britney';
+// const lastName0 = 'Gears';
+// console.log(`${firstName0} ${lastName0}`);
 
-const firstName2 = 'Ted';
-const lastName2 = 'Bundy';
+// const firstName2 = 'Ted';
+// const lastName2 = 'Bundy';
 
 
 const namePrinter = (firstName, lastName) => {
@@ -18,3 +18,9 @@ console.log(`${firstName} ${lastName}`);
 
 namePrinter('Austin', 'Casey');
 namePrinter('Ted', 'Bungus');
+
+const nuggetizer = (animal) =>{ 
+return `processed ${animal}`;
+};
+
+console.log(nuggetizer('peeg'));
